29e65ca6c3bffd1d029edda36430d9778f2fcef6
jclucasasp <70893888jclucasasp@github.com>
2021-05-27 12:46:42 +0200

    Feature added

---
+++ components/greeting.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+export class Greeting extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: ''}
+    }
+
+render(){
+    return(
+        <View style={StyleSheet.container}>
+            <TextInput style={{height:30, borderColor: 'gray', borderWidth:1}} 
+            placeholder="Write your name..." onChangeText={(name) => this.setState({name})}/>
+        <Text style={{marginTop:10}}>Welcome {this.state.name}</Text>
+        </View>
+    )
+}
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
